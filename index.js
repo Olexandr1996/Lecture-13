87f8b5f5283a1c63efbc738548c0c308bbad5b66
Olexandr1996 <longervs@gmail.com>
2021-11-21 11:43:34 +0200

    first task

---
+++ index.js
@@ -0,0 +1,58 @@
+//let diferentNumber = prompt ('Enter number : ') 
+//let numer = [
+   //'zero', 
+   //'one',  
+   //'two',
+   //'three',
+   //'four',
+   //'five',
+   //'six',
+   //'seven',
+   //'eight',
+  // 'nine',
+
+
+//]; 
+ 
+//function check (number) {
+  //if (+number >= 0 && +number <=9) {
+    //return numer[+number];
+  //} else {
+    //'Something going wrong';
+  //}
+//} 
+//alert(check(diferentNumber)) 
+
+
+
+
+//function add (x){
+  //return function (y) {
+    //return x + y;
+  //} 
+//} 
+//alert (add(3)(4)) 
+
+
+
+
+
+
+
+const person = {
+  firstName: "Olexandr",
+  lastName: "Diadiuk",
+  fullName: function() {
+    return  this.firstName + " lastName is  " + this.lastName + "  I am very exited to learn Javascript";
+  }
+}; 
+alert ("Hello my firstName is" + person.fullName())
+
+
+
+
+
+
+
+
+
